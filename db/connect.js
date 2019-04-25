2675e30ac46c2c5179421f469deb32cfa203549c
antonio.hadrovic <hadrovic1@gmail.com>
2019-04-25 16:40:20 +0200

    Add DB credentials

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: '', //marinokol
-  host: '', //138.68.87.73
-  database: '', // db_marinokol
-  password: '', // your password
+  user: 'antonioha', //marinokol
+  host: '138.68.87.73', //138.68.87.73
+  database: 'db_antonioha', // db_marinokol
+  password: '#4Z?my@8', // your password
   port: 5432, // 5432
 })
 
@@ -13,4 +13,11 @@ module.exports = {
     return pool.query(text, params, callback);
   },
 }
+
+//test
+
+pool.query('SELECT * FROM HALLS', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
